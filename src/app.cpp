760e19389b998b6188296ff9b55ede41b27b9785
Lesley Lai <lesley@lesleylai.info>
2021-03-06 13:04:52 -0700

    Setup Vulkan device

--- src/app.cpp
+++ src/app.cpp
@@ -1,9 +1,41 @@
 #include "app.hpp"
 
+#include <VkBootstrap.h>
 #include <fmt/format.h>
 
 #define CHECK_GLFW(glfw_call)                                                  \
-  if (!(glfw_call)) std::exit(1);
+  if (!(glfw_call)) { std::exit(1); }
+
+#define VK_CHECK(x)                                                            \
+  do {                                                                         \
+    VkResult err = x;                                                          \
+    if (err) {                                                                 \
+      fmt::print("Vulkan error: {}\n", err);                                   \
+      exit(-1);                                                                \
+    }                                                                          \
+  } while (0)
+
+namespace {
+
+auto create_surface_glfw(VkInstance instance, GLFWwindow* window)
+    -> VkSurfaceKHR
+{
+  VkSurfaceKHR surface = VK_NULL_HANDLE;
+  VkResult err = glfwCreateWindowSurface(instance, window, nullptr, &surface);
+  if (err) {
+    const char* error_msg = nullptr;
+    int ret = glfwGetError(&error_msg);
+    if (ret != 0) {
+      fmt::print("{} ", ret);
+      if (error_msg != nullptr) { fmt::print("{}", error_msg); }
+      fmt::print("\n");
+    }
+    surface = VK_NULL_HANDLE;
+  }
+  return surface;
+}
+
+} // namespace
 
 App::App()
 {
@@ -14,10 +46,19 @@ App::App()
   window_ = glfwCreateWindow(1400, 900, "Voxel Game", nullptr, nullptr);
   if (!window_) { std::exit(1); }
   glfwMakeContextCurrent(window_);
+
+  init_vk_device();
 }
 
 App::~App()
 {
+  if (!device_) { return; }
+
+  vkDestroyDevice(device_, nullptr);
+  vkDestroySurfaceKHR(instance_, surface_, nullptr);
+  vkb::destroy_debug_utils_messenger(instance_, debug_messenger_, nullptr);
+  vkDestroyInstance(instance_, nullptr);
+
   glfwDestroyWindow(window_);
   glfwTerminate();
 }
@@ -28,4 +69,37 @@ void App::exec()
     glfwSwapBuffers(window_);
     glfwPollEvents();
   }
+}
+
+void App::init_vk_device()
+{
+
+  auto instance_ret = vkb::InstanceBuilder{}
+                          .use_default_debug_messenger()
+                          .request_validation_layers()
+                          .build();
+  if (!instance_ret) {
+    fmt::print("{}\n", instance_ret.error().message());
+    std::exit(-1);
+  }
+  instance_ = instance_ret->instance;
+  debug_messenger_ = instance_ret->debug_messenger;
+  surface_ = create_surface_glfw(instance_, window_);
+
+  vkb::PhysicalDeviceSelector phys_device_selector(instance_ret.value());
+  auto phys_device_ret = phys_device_selector.set_surface(surface_).select();
+  if (!phys_device_ret) {
+    fmt::print("{}\n", phys_device_ret.error().message());
+    std::exit(-1);
+  }
+  vkb::PhysicalDevice vkb_physical_device = phys_device_ret.value();
+  physical_device_ = vkb_physical_device.physical_device;
+
+  vkb::DeviceBuilder device_builder{vkb_physical_device};
+  auto device_ret = device_builder.build();
+  if (!device_ret) {
+    fmt::print("{}\n", device_ret.error().message());
+    std::exit(-1);
+  }
+  device_ = device_ret->device;
 }
--- src/app.hpp
+++ src/app.hpp
@@ -1,10 +1,19 @@
 #ifndef VOXEL_GAME_APP_HPP
 #define VOXEL_GAME_APP_HPP
 
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
 class App {
   GLFWwindow* window_ = nullptr;
+  VkInstance instance_{};
+  VkDebugUtilsMessengerEXT debug_messenger_{};
+  VkSurfaceKHR surface_{};
+  VkPhysicalDevice physical_device_{};
+  VkDevice device_{};
+  VkQueue graphics_queue_{};
+  VkQueue present_queue_{};
 
 public:
   App();
@@ -16,6 +25,9 @@ public:
   auto operator=(const App&) -> App& = delete;
   App(App&&) noexcept = delete;
   auto operator=(App&&) noexcept -> App& = delete;
+
+private:
+  void init_vk_device();
 };
 
 #endif // VOXEL_GAME_APP_HPP
