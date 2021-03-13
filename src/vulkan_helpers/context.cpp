b3b57dc8900ae5089bf80da582e3162a423ce78b
Lesley Lai <lesley@lesleylai.info>
2021-03-12 20:02:30 -0700

    Add fillModeNonSolid to required features

--- src/vulkan_helpers/context.cpp
+++ src/vulkan_helpers/context.cpp
@@ -49,7 +49,11 @@ Context::Context(Window& window)
   surface_ = create_surface_glfw(instance_, window.glfw_window());
 
   vkb::PhysicalDeviceSelector phys_device_selector(instance_ret.value());
-  auto phys_device_ret = phys_device_selector.set_surface(surface_).select();
+  auto phys_device_ret = phys_device_selector.set_surface(surface_)
+                             .set_required_features(VkPhysicalDeviceFeatures{
+                                 .fillModeNonSolid = true,
+                             })
+                             .select();
   if (!phys_device_ret) {
     fmt::print("{}\n", phys_device_ret.error().message());
     std::exit(-1);
